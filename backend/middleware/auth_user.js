01fdd0a3d1bf5f70eff8df2378b52ddf1f6239af
ZyrthazZ <jeanseb.chevalier@gmail.com>
2022-04-18 20:07:18 +0200

    Début de mise à jour du middleware d'authentification auth_user

--- backend/middleware/auth_user.js
+++ backend/middleware/auth_user.js
@@ -22,31 +22,81 @@ module.exports = async (req, res, next) => {
 
         console.log("userId", userId)
         console.log("isAdmin", isAdmin)
+        console.log("req.params.courgetteId", req.params.courgetteId)
+        console.log("req.params.tomateId", req.params.tomateId)
 
-        /* if (isAdmin) {
+        if (isAdmin) {
             console.log("isAdmin")
+            req.isAdmin
             next();
         } else {
             console.log("isNotAdmin")
-        } */
+            //Check if there is a req.params.tomateId in the request
+            if (req.params.tomateId) {
+                console.log("there is a tomateId")
+                const checkUserId = await models.User.findOne({
+                    where: {
+                        id: userId
+                    }
+                });
 
-        const checkUserId = await models.User.findOne({
-            where: {
-                id: userId
+                console.log("checkUserId", checkUserId);
+                //Let's check if there is userId in the checkUserId object and if he is different 
+                //from the token extracted userId
+                if (checkUserId.id && checkUserId.id !== userId) { // User login is not user that owns post
+                    throw "Invalid user ID";
+                } else { // User login is user that owns post
+                    console.log("Authentified request with the tomateId !")
+                    next();
+                }
+                //Let's check if the post contains a userId equal to the extracted userId
+            } else {
+                //Check if there is a req.params.courgetteId in the request
+                if (req.params.courgetteId) {
+                    console.log("there is a courgetteId")
+                    if (req.params.courgetteId == userId) {
+                        console.log("les id correspondent !")
+                        const checkUserId = await models.User.findOne({
+                            where: {
+                                id: userId
+                            }
+                        });
+
+                        console.log("checkUserId", checkUserId);
+                        //Let's check if there is userId in the checkUserId object and if he is different 
+                        //from the token extracted userId
+                        if (checkUserId.id && checkUserId.id !== userId) { // User login is not user that owns post
+                            throw "Invalid user ID";
+                        } else { // User login is user that owns post
+                            console.log("Authentified request with the courgetteId!")
+                            next();
+                        }
+                        //Let's check if the post contains a userId equal to the extracted userId
+                    } else {
+                        throw "Invalid user ID";
+                    }
+                } else { //If there is no req.params.courgetteId
+                    console.log("no courgetteId or tomateId")
+
+                    const checkUserId = await models.User.findOne({
+                        where: {
+                            id: userId
+                        }
+                    });
+
+                    console.log("checkUserId", checkUserId);
+                    //Let's check if there is userId in the checkUserId object and if he is different 
+                    //from the token extracted userId
+                    if (checkUserId.id && checkUserId.id !== userId) { // User login is not user that owns post
+                        throw "Invalid user ID";
+                    } else { // User login is user that owns post
+                        console.log("Authentified request !")
+                        next();
+                    }
+                }
             }
 
-        });
-
-        console.log("checkUserId", checkUserId);
-        //Let's check if there is userId in the checkUserId object and if he is different 
-        //from the token extracted userId
-        if (checkUserId.id && checkUserId.id !== userId) { // User login is not user that owns post
-            throw "Invalid user ID";
-        } else { // User login is user that owns post
-            console.log("Authentified request !")
-            next();
-        }
-        //Let's check if the post contains a userId equal to the extracted userId
+        } //End of if (isAdmin)
     } catch (err) {
         console.log('Error in auth_user : ', err)
         res.status(401).json({
